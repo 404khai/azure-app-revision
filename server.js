91cc7a344b225d0b24143e831652659e64e255b0
Khai <omajeneoghenefega11@gmail.com>
2026-08-10 12:26:15 +0100

    feat: initial azure app service application

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>Hello from Azure App Service!</h1>
+        <p>This application is running on Azure.</p>
+    `);
+});
+
+app.get("/students", (req, res) => {
+    res.json([
+        { name: "Student 1", course: "Python" },
+        { name: "Student 2", course: "Java" }
+    ]);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
